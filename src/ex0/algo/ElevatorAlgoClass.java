af4f9e9becc705dcadd123e14a05252b5ee54f27
Artem Shabalin <tomshabalin95@gmail.com>
2021-10-26 16:12:57 +0300

    Commit message : have done some changes. call,getDirection method's has been added. Need to work hard on this ANNNNNNAAA

--- src/ex0/algo/ElevatorAlgoClass.java
+++ src/ex0/algo/ElevatorAlgoClass.java
@@ -5,20 +5,25 @@ import ex0.CallForElevator;
 import ex0.Elevator;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class ElevatorAlgoClass implements ElevatorAlgo {
     private final int UP=1,DOWN=-1;
     private Building building;
-    private int direction;
-    private List<Elevator> elevators = new ArrayList<Elevator>();
 
-    //constructor
-    public ElevatorAlgoClass(Building name){
-        this.building = name;
+    private int direction;
+    private ArrayList<Elevator> elevatorsArr;
+    private ArrayList<CallForElevator> calls;
+
+    /*
+    class constructor
+     */
+    public ElevatorAlgoClass(Building building){
+        this.building = building;
         this.direction = UP;
-        this.elevators = new ArrayList<>(this.building.numberOfElevetors());
+        int amountOfFloors = (this.building.maxFloor() - this.building.minFloor()) + 1; //amount of floors
+        this.elevatorsArr = new ArrayList<>(building.numberOfElevetors());    //initialize arr with the number of elevators
+        this.calls = new ArrayList<CallForElevator>();  //initialize arr with amount of calls
     }
 
     @Override
@@ -28,23 +33,19 @@ public class ElevatorAlgoClass implements ElevatorAlgo {
 
     @Override
     public String algoName() {
-        return "The best algo!";
+        return "Ex0_Elevators_Algorithm";
     }
 
     @Override
     public int allocateAnElevator(CallForElevator c) {
-        if(elevators.isEmpty())
+        if(elevatorsArr.isEmpty())
             throw new NoSuchElementException("No elevators in this building");
-        //if there are only one elevator int the building
-        if(elevators.size() == 1){
-            //we will split it to two sections - up and down
-            //up-->
-            if(c.getSrc() < c.getDest())
-                return 0;
-
+        double time = Integer.MAX_VALUE;
+        int k = 0;
+        for(int i = 0 ; i < elevatorsArr.size() ; i++){
+            Elevator e = this.building.getElevetor(i);
 
         }
-        return 0;
     }
 
     @Override
@@ -52,21 +53,29 @@ public class ElevatorAlgoClass implements ElevatorAlgo {
 
     }
 
-    private int ArrivingTime(int source,Elevator index){
-        int result = 1;
-        double speed = index.getSpeed();
-        int position = index.getPos();
-        double stopTime = index.getStopTime();
-        double startTime = index.getStartTime();
-        double timeForOpen = index.getTimeForOpen();
-        double timeForClose = index.getTimeForClose();
-        int maxFloor = this.building.maxFloor(),mintFloor = this.building.minFloor();
-        double interval = (maxFloor-mintFloor) * speed;
-        double fromTo = (stopTime + startTime + timeForClose + timeForOpen) * (Math.abs(source-position));
-        //split to up and down -->
-        if(position <= source){
-            result = (int)fromTo + (int)interval;
-        }
+    //getting the direction of given elevator
+    public int getDirection(CallForElevator c) {
+        /*
+        we will check the conditions of each call same as in the 'call' method
+         */
+        //if the elevator is on his way to some floor
+        if(c.getState() == 2)
+            return c.getDest();
+        //if the elevator is on his way to toe source floor || he is arrived
+        if(c.getState() == 1 || c.getState() == 0)
+            return c.getSrc();
+        //in case that the elevator is in "rest" mood
         return 0;
     }
+
+    //method to initialize a call for specific elevator
+    private void call(Elevator e,CallForElevator c){
+        //going to destination
+        if(c.getState() == 2)
+            e.goTo(c.getDest());
+        //if the elevator is on the way to the source floor or arrived
+        else if(c.getState() ==1 || c.getState() == 0)
+            e.goTo(c.getDest());
+    }
+
 }
